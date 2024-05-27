46a41e76bc512487347d3e33514666a67593c3d9
devasuryapradeep <devasuryapradeep4@gmail.com>
2024-05-27 15:14:36 +0530

    Fetched images from the API in a synchronous way by implementing  async let .

--- asyncAwaitBootcamp/Async let/AsyncletView.swift
+++ asyncAwaitBootcamp/Async let/AsyncletView.swift
@@ -19,21 +19,19 @@ struct AsyncletView: View {
                         Image(uiImage: returnedImage)
                             .resizable()
                             .scaledToFit()
-                            .frame(width: 100,height: 50)
+                            .frame(width: 180,height: 200)
                     }
                 }
             }
             .navigationTitle(Text("Async let."))
             .task {
                 do{
-                    let image1 = try await viewModelInstance.downloadImage()
-                    viewModelInstance.dataArray.append(image1)
-                    let image2 = try await viewModelInstance.downloadImage()
-                    viewModelInstance.dataArray.append(image2)
-                    let image3 = try await viewModelInstance.downloadImage()
-                    viewModelInstance.dataArray.append(image3)
-                    let image4 =  try await viewModelInstance.downloadImage()
-                    viewModelInstance.dataArray.append(image4)
+                    async let fetchedImage1 = viewModelInstance.downloadImage()
+                    async let fetchedImage2 = viewModelInstance.downloadImage()
+                    async let fetchedImage3 = viewModelInstance.downloadImage()
+                    async let fetchedImage4 = viewModelInstance.downloadImage()
+               let (image1,image2,image3,image4) = try await (fetchedImage1,fetchedImage2,fetchedImage3,fetchedImage4)
+                    viewModelInstance.dataArray.append(contentsOf: [image1,image2,image3,image4])
                 }
                 catch {
                     print(error.localizedDescription)
@@ -49,8 +47,8 @@ struct AsyncletView: View {
 
 //Series manner of executing the task.
 /* This is a way through which the images are fetched from the API and async  await pattern and on calling the series of functionality inside the task it will get executed in series manner [One after the other ]. Even if you any of the async chronus functiuo failed then the catch block is executed. If you want to av oid that situation you can use try? instead which will be marking a possiblity failure result while executing and on when one gets failed then the it is skipped and rest is executed */
- 
+
 //Parallel manner of executing the tasks.
 /*
- 
+ On using Async let we are performing the multiple APi fetch all together at the same time but awaiting the result all together. On using this method every image can be displayed altogether without any delay.
  */

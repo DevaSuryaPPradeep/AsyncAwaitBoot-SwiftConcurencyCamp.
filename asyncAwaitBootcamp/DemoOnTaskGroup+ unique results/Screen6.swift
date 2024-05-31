96ef8ac22d2a96e9e2cfa24e5fe4056c0f86e746
devasuryapradeep <devasuryapradeep4@gmail.com>
2024-05-31 11:30:25 +0530

    Few updates ...

--- asyncAwaitBootcamp/DemoOnTaskGroup+ unique results/Screen6.swift
+++ asyncAwaitBootcamp/DemoOnTaskGroup+ unique results/Screen6.swift
@@ -1,53 +1,55 @@
 import SwiftUI
 
+/// View.
 struct Screen6: View {
     
+    /// State object decalarations
     @StateObject private var demoViewModel = DemoViewModel()
+    
+    /// State property declarations.
+    @State private var isLoading: Bool = false
     
     var body: some View {
         ScrollView {
-                VStack {
-                        Text("Coin Details")
-                            .font(.headline)
-                        // List of coin details
-                        ForEach(demoViewModel.dataProvider.first?.coinInfo ?? [], id: \.self) { coin in
-                            VStack {
-                                Text("Name: \(coin.name)")
-                                Text("Symbol: \(coin.symbol)")
-                            }
-                            .padding()
-                            .background(Color.gray.opacity(0.1))
-                            .cornerRadius(10)
-                            .padding(.horizontal)
+            VStack {
+                Text("Coin Details")
+                    .font(.headline)
+                // List of coin details
+                ForEach(demoViewModel.dataProvider.first?.coinInfo ?? [], id: \.self) { coin in
+                    VStack {
+                        Text("Name: \(coin.name)")
+                        Text("Symbol: \(coin.symbol)")
+                    }
+                    .padding()
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(10)
+                    .padding(.horizontal)
+                }
+                VStack(alignment: .leading) {
+                    Text("Job Details")
+                        .font(.headline)
+                    // List of job details
+                    ForEach(demoViewModel.dataProvider.first?.jobInfo ?? [], id: \.self) { job in
+                        VStack(alignment: .leading) {
+                            Text("Title: \(job.title)")
+                            Text("Department: \(job.department)")
+                            Text("Posted Date: \(job.postedDate)")
+                            Text("Deadline Date: \(job.deadlineDate)")
+                            Text("Description: \(job.description)")
+                            Text("Salary: \(job.salary)")
+                            Text("Status: \(job.status)")
+                            Text("Responsibilities: \(job.responsibilities)")
                         }
-                    VStack(alignment: .leading) {
-                        Text("Job Details")
-                            .font(.headline)
-                        // List of job details
-                        ForEach(demoViewModel.dataProvider.first?.jobInfo ?? [], id: \.self) { job in
-                            VStack(alignment: .leading) {
-                                Text("Title: \(job.title)")
-                                Text("Department: \(job.department)")
-                                Text("Posted Date: \(job.postedDate)")
-                                Text("Deadline Date: \(job.deadlineDate)")
-                                Text("Description: \(job.description)")
-                                Text("Salary: \(job.salary)")
-                                Text("Status: \(job.status)")
-                                Text("Responsibilities: \(job.responsibilities)")
-                            }
-                            .padding()
-                            .background(Color.gray.opacity(0.1))
-                            .cornerRadius(10)
-                            .padding(.horizontal)
-                        }
+                        .padding()
+                        .background(Color.gray.opacity(0.1))
+                        .cornerRadius(10)
+                        .padding(.horizontal)
                     }
                 }
-            
-        }
-        .onAppear {
-            Task {
+            }
+            .task {
                 do {
-                  let _ =  try await demoViewModel.fetchDetails()
+                    let _ =  try await demoViewModel.fetchDetails()
                 } catch {
                     print("Failed to fetch details: \(error.localizedDescription)")
                 }
